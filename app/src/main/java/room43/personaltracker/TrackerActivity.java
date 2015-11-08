fc015d477a215c5226661715c341e466e6730ca8
Rusetskiy Alex <rusetskyalex@gmail.com>
2015-11-08 22:27:18 +0300

    changed TrackerActivity Main (i = 3)

--- app/src/main/java/room43/personaltracker/TrackerActivity.java
+++ app/src/main/java/room43/personaltracker/TrackerActivity.java
@@ -1,13 +1,16 @@
 package room43.personaltracker;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class TrackerActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        int i = 1 + 2;
+
+        
         setContentView(R.layout.activity_tracker);
     }
 }
